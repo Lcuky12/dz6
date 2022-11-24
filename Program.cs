d3a6640cf0773a4946329f5da9f53c34326ba964
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2022-11-24 21:34:05 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace домашка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+            int time = 20;
+            Console.WriteLine(age);
+            Console.WriteLine(time);
+            int temporaryAge;
+            temporaryAge = time;
+            time = age;
+            Console.WriteLine(temporaryAge);
+            Console.WriteLine(time);
+        }
+    }
+}
